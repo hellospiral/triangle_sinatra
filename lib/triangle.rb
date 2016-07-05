7b51a80125f859b8a97f1a61702b72fa5c80c6c9
James Turello and Matt Carlson <matt@>
2016-07-05 09:02:22 -0700

    set up unit tests for triangle object

---
+++ lib/triangle.rb
@@ -0,0 +1,22 @@
+class Triangle
+  define_method(:initialize) do |a, b, c|
+    @side_a = a
+    @side_b = b
+    @side_c = c
+
+  end
+
+  define_method(:triangle_type) do
+    if (@side_a + @side_b < @side_c) || (@side_b + @side_c < @side_a) || (@side_a + @side_c < @side_b)
+      "not a triangle"
+    else
+      if @side_a == @side_b && @side_b == @side_c && @side_a == @side_c
+        "equilateral"
+      elsif (@side_a == @side_b && @side_a != @side_c) || (@side_a == @side_c && @side_b != @side_c) || (@side_b == @side_c && @side_a != @side_c)
+        "isosceles"
+      elsif @side_a != @side_b && @side_b != @side_c && @side_a != @side_c
+        "scalene"
+      end
+    end
+  end
+end
---
+++ spec/triangle_spec.rb
@@ -0,0 +1,28 @@
+require('rspec')
+require('triangle')
+
+describe(Triangle) do
+  describe('#triangle_type') do
+
+    it('returns "equilateral" if all sides are equal') do
+      new_triangle = Triangle.new(2, 2, 2)
+      expect(new_triangle.triangle_type()).to(eq("equilateral"))
+    end
+
+    it('returns "isosceles" if 2 sides are equal') do
+      new_triangle = Triangle.new(2, 2, 3)
+      expect(new_triangle.triangle_type()).to eq("isosceles")
+    end
+
+    it('returns "scalene" if no sides are equal') do
+      new_triangle = Triangle.new(5, 4, 3)
+      expect(new_triangle.triangle_type()).to eq("scalene")
+    end
+
+    it('returns "not a triangle" if any two sides add up to less than the third side') do
+      new_triangle = Triangle.new(44, 2, 1)
+      expect(new_triangle.triangle_type()).to eq("not a triangle")
+    end
+
+  end
+end
